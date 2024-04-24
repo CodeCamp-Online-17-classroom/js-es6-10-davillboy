922842776e16d1b29073a4df2ff4c1a81387aff0
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 16:50:29 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+function cloneObject(obj) {
+  return { ...obj };
+}
+
+const originalObject = { a: 1, b: 2, c: 3 };
+const clonedObject = cloneObject(originalObject);
+
+console.log(originalObject); 
+console.log(clonedObject); 
